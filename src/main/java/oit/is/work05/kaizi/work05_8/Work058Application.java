1fa6eff8c9781f88ed2745fe015a043045c1e588
e1b20043 <e1b20043@oit.ac.jp>
2022-10-25 13:52:36 +0900

    初期化

---
+++ src/main/java/oit/is/work05/kaizi/work05_8/Work058Application.java
@@ -0,0 +1,13 @@
+package oit.is.work05.kaizi.work05_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work058Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work058Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/work05/kaizi/work05_8/Work058ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.work05.kaizi.work05_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work058ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
